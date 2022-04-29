3c7c529c7b90e1af2e682f1d8417a5cd71fa113b
Caily.Thinh <thinh_web@caily.com.vn>
2022-04-29 13:07:35 +0700

    edit mv

--- files/user/lp/boosterclub/js/scripts.js
+++ files/user/lp/boosterclub/js/scripts.js
@@ -1,3 +1,13 @@
 $(document).ready(function($) {
-	console.log("FUckyoui");
+	jsMainVisual();
 });//End -> ready method
+
+// mainvisual
+function jsMainVisual(){
+	var hHeader = $('.header').outerHeight();
+	var hSlogan = $('.box-mv__slogan').outerHeight();
+	var imgMV = $('.box-mv__main__img img');
+	imgMV.css({
+		'height' : 'calc(100vh - ' + hHeader + 'px' + ' - ' + hSlogan + 'px)'
+	});
+}
