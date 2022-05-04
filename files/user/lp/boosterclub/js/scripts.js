e7ba3545fa7225c2054c5e6aab40122f95fdb92c
Caily.Thinh <thinh_web@caily.com.vn>
2022-05-04 07:11:59 +0700

    edit menu and mv

--- files/user/lp/boosterclub/js/scripts.js
+++ files/user/lp/boosterclub/js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function($) {
 	jsMainVisual();
+	jsNav();
 });//End -> ready method
 
 // mainvisual
@@ -10,4 +11,27 @@ function jsMainVisual(){
 	imgMV.css({
 		'height' : 'calc(100vh - ' + hHeader + 'px' + ' - ' + hSlogan + 'px)'
 	});
+}
+
+// Navigation
+function jsNav(){
+	var btnOpen = $('.header-trigger');
+	var btnClose = $('.nav__close');
+	var overlay = $('.nav__overlay');
+	var content = $('.nav__content');
+	var nav = $('.nav');
+	
+	nav.hide();
+	btnOpen.on('click', function() {
+		nav.show(200);
+		nav.addClass('is-open');
+		
+	});
+	btnClose.on('click', function() {
+		nav.removeClass('is-open');
+		setTimeout(function(){
+			nav.hide();
+		}, 400);
+	});
+	
 }
